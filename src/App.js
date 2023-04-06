f380a0303ba8b91f6a5904c14dec6db6be60460c
Saman Daneshmayeh <daneshmayeh.saman@gmail.com>
2023-04-06 18:32:58 +0430

    Details

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 // Components
 import Store from './components/Store';
 
 // Context
 import ProductContextProvider from './context/ProductContextProvider';
+import ProductDetail from './components/ProductDetail';
 
 function App() {
   return (
     <ProductContextProvider>
-      <Store />
+      <Switch>
+        <Route path="/products/:id" component={ProductDetail} />
+        <Route path="/products" component={Store} />
+        <Redirect to="/products" />
+      </Switch>
     </ProductContextProvider>
   );
 }
