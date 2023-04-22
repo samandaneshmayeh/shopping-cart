19d4650d15426f4371b3812d91fcdbe24d3259a0
Saman Daneshmayeh <daneshmayeh.saman@gmail.com>
2023-04-22 23:53:01 +0430

    EDITED

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import './App.css';
-import { Switch, Route, Redirect } from 'react-router-dom';
-
-// Components
-import Store from './components/Store';
-import ProductDetail from './components/ProductDetail';
-
-// Context
-import ProductContextProvider from './context/ProductContextProvider';
-import CartContextProvider from './context/CartContextProvider';
 
 function App() {
   return (
-    <ProductContextProvider>
-      <CartContextProvider>
-        <Switch>
-          <Route path="/products/:id" component={ProductDetail} />
-          <Route path="/products" component={Store} />
-          <Redirect to="/products" />
-        </Switch>
-      </CartContextProvider>
-    </ProductContextProvider>
+    <div>
+      Hello
+    </div>
   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
